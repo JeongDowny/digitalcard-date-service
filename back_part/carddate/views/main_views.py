c6343ad4ba0f545ac19417dd3efb5d1506bc2be8
문효재 <ansgywo0617@naver.com>
2024-10-07 19:46:54 +0900

    Card 데이터 SQLite에 저장

--- back_part/carddate/views/main_views.py
+++ back_part/carddate/views/main_views.py
@@ -8,7 +8,3 @@ bp = Blueprint('main', __name__, url_prefix='/')
 @bp.route('/hello')
 def hello_pybo():
     return 'Hello, Pybo!'
-
-@bp.route('/')
-def index():
-    return redirect(url_for('question._list'))
